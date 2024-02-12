6954d3f178ec05b0183afb351d7e9ef48a28c710
earthpywt <piyawat.kus@gmail.com>
2024-02-12 22:47:06 +0700

    refactor: :recycle: remove pet form component from profile page
    The component was added for testing. Its presence was unintentional.

--- petpal/src/app/(routes)/profile/page.tsx
+++ petpal/src/app/(routes)/profile/page.tsx
@@ -1,11 +1,5 @@
 import React from "react";
-import CreatePet from "./_components/CreatePetForm";
 
 export default function Profile() {
-    return (
-        <div>
-            Profile
-            <CreatePet />
-        </div>
-    );
+    return <div>Profile</div>;
 }
