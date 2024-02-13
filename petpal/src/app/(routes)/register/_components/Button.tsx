bdaaacc4fda622cb2860da5e31f30b1dbdee5fc0
preamza02 <79450371+preamza02@users.noreply.github.com>
2024-02-13 21:55:12 +0700

    Revert "Dev us 1 6"

--- petpal/src/app/(routes)/register/_components/Button.tsx
+++ petpal/src/app/(routes)/register/_components/Button.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
 
-export default function Button({name}:{name:string}) {
+export default function Button() {
   return (
-    <button className="m-3 py-2 px-5 bg-[#FF872F] text-white font-semibold rounded-full shadow-md hover:bg-orange-500 
-      focus:outline-none focus:ring focus:ring-orange-400 focus:ring-opacity-75">
-      {name}
-    </button>
+    <div>Button</div>
   )
 }
