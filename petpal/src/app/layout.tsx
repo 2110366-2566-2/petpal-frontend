d49ff76fefa9bc1f8add9f762c934680f3f268fd
MisterStank <chakkritchongkraichak@gmail.com>
2024-02-14 11:23:47 +0700

    fix(add navbar to layout): :art:

--- petpal/src/app/layout.tsx
+++ petpal/src/app/layout.tsx
@@ -1,8 +1,15 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NavBar from "./_component/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const navitems = [
+  {name : "Listing" , link : "/listing"},
+  {name : "Booking" , link : "/booking"},
+  {name : "Profile" , link : "/profile"}
+]
 
 export const metadata: Metadata = {
   title: "PetPal",
@@ -16,7 +23,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NavBar brandName={"PETPAL"} navItems={navitems}/>
+        {children}
+      </body>
     </html>
   );
 }
