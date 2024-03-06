1bf78e34aa213d8f50a0ee3c44b98389f4a0333b
earthpywt <piyawat.kus@gmail.com>
2024-03-06 18:52:39 +0700

    refactor: move login and register API calls to libs folder

---
+++ petpal/src/app/libs/registerUser.tsx
@@ -0,0 +1,42 @@
+export default async function registerUser(
+    address: string,
+    dateOfBirth: string,
+    email: string,
+    fullName: string,
+    password: string,
+    phoneNumber: string,
+    username: string
+) {
+    const response = await fetch("http://localhost:8080/register-user", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            address: address,
+            dateOfBirth: dateOfBirth + "T00:00:00Z",
+            email: email,
+            fullName: fullName,
+            password: password,
+            phoneNumber: phoneNumber,
+            username: username,
+        }),
+    });
+
+    if (response.ok) {
+        // Register successful
+        console.log("Register successful");
+        console.log(response.status);
+    } else {
+        // Register failed
+        console.error("Register failed");
+        try {
+            const errorMessage = await response.text();
+            console.error("Error message:", errorMessage);
+        } catch (error) {
+            console.error("Failed to parse error message:", error);
+        }
+    }
+
+    return await response.json();
+}
