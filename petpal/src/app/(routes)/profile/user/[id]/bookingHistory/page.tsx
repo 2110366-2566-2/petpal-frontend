31148d2ce8e52df53f18ce3ef34716260e8b013c
earthpywt <piyawat.kus@gmail.com>
2024-03-06 17:54:09 +0700

    feat: initialize booking history page

---
+++ petpal/src/app/(routes)/profile/user/[id]/bookingHistory/page.tsx
@@ -0,0 +1,25 @@
+export default function BookingHistory() {
+    return (
+        <main>
+            <div>
+                <div className="flex">
+                    <div>Service Name</div>
+                    <div>completed</div>
+                </div>
+                <div>Provider Name</div>
+                <div className="flex">
+                    <div>Feb 7</div>
+                    <div>14.00 - 16.00</div>
+                </div>
+                <div className="flex">
+                    <div>500฿</div>
+                    <div className="flex">
+                        <div>Reschedule</div>
+                        <div>Cancel</div>
+                    </div>
+                </div>
+                <div>Write Feedback</div>
+            </div>
+        </main>
+    );
+}
