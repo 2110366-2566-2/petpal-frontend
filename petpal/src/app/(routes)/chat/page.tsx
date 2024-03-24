28b2b8eb33e465a2f0f7a49c3195d4252efc941f
pream <preamza02@gmail.com>
2024-03-24 15:51:28 +0700

    feat: try to call api for chat historyuser interface

--- petpal/src/app/(routes)/chat/page.tsx
+++ petpal/src/app/(routes)/chat/page.tsx
@@ -1,18 +1,15 @@
 "use client"
 import React from "react";
-// import HeaderChatComponent from "./_components/HeaderChatComponent";
-// import ChatPreview from "./_components/ChatPreview";
-// import ChatPageinterface from "./_interface/ChatPageInterface";
-// import ChatHistoryUserInterface from "./_interface/ChatHistoryUserInterface";
-// import { ExampleChatPageUser1 } from "./_interface/ChatPageInterface";
-// import ChatSearchBar from "./_components/ChatSearchBar";
-import { redirect } from 'next/navigation';
-import { usePathname } from 'next/navigation';
+import { useRouter } from "next/navigation";
+// import { redirect } from 'next/navigation';
+// import { usePathname } from 'next/navigation';
 
 export default function ChatPage() {
     // var ChatPageUser: ChatPageinterface = ExampleChatPageUser1
-    const currentPage = usePathname();
-    redirect(currentPage + "/0")
+    // const currentPage = usePathname();
+    // redirect(currentPage + "/0")
+    const router = useRouter()
+    router.push('')
     // return (
     //     <div className="">
     //         <div className="md:max-w-[450px] md:h-screen outline-8 border-solid border-r-2 h-full md:p-[30px] md:pr-[10px]">
