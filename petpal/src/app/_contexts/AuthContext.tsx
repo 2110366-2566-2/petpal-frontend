328a401408c23a1f10300eafe1d3ac02fe96ff6d
naphattar <naphatchalermwai@gmail.com>
2024-03-25 09:58:02 +0700

    feat(AuthContext.tsx): :sparkles: create AuthContext

---
+++ petpal/src/app/_contexts/AuthContext.tsx
@@ -0,0 +1,75 @@
+"use client";
+import { ReactNode, createContext, useEffect, useState } from "react";
+import { User } from "../_interface/user/user";
+import { Svcp } from "../_interface/svcp/svcp";
+import { getCurrentEntity } from "../libs/user/userBackend";
+import { getCurrentEntityType } from "../libs/currentEntiity/getCurrentEntityType";
+
+type CurrentEntity = User | Svcp;
+
+interface TAuthContext {
+  currentEntity: CurrentEntity | null;
+  isLogin : Boolean;
+  setCurrentEntity : (currentEntity: CurrentEntity | null) => void;
+  setIsLogin : (isLogin : Boolean ) => void;
+}
+
+export const AuthContext = createContext<TAuthContext>({
+    currentEntity: null,
+    setCurrentEntity: () => {},
+    isLogin: false,
+    setIsLogin: () => {},
+});
+
+interface Props {
+  children: ReactNode;
+}
+
+export const AuthProvider = ({ children }: Props) => {
+  const [currentEntity, setCurrentEntity] = useState<CurrentEntity | null>(null);
+  const [isLogin ,setIsLogin] = useState<Boolean>(false);
+  const [accType, setAccType] = useState<string>("waiting")
+  
+  useEffect(() => {
+    getCurrentEntity().then((Response) => {
+        setCurrentEntity(Response);
+        setAccType(getCurrentEntityType(Response))
+        console.log("Current enitity : ",Response)
+    });
+  }, []);
+  useEffect(() => {
+    // console.log(accType)
+    switch (accType) {
+        case "waiting": {
+            console.log("waiting")
+            break
+        }
+        case "user": {
+            console.log("user")
+            setIsLogin(true);
+            break
+        }
+        case "svcp": {
+            console.log("svcp")
+            setIsLogin(true);
+            break
+        }
+        case "undefined": {
+            console.log("undefined")
+            setIsLogin(false);
+            break
+        }
+        default: {
+            console.log("error")
+            setIsLogin(false);
+            break
+        }
+        }
+  }, [accType]);
+
+  return (
+    <AuthContext.Provider value={{ currentEntity, setCurrentEntity ,isLogin ,setIsLogin}}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
