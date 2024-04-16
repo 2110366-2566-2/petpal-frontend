d205bad70ba644aa563bf5306f2d67c9bf762372
MisterStank <chakkritchongkraichak@gmail.com>
2024-04-16 20:34:05 +0700

    feat: :sparkles: verify daila

--- petpal/src/app/(routes)/admin/verify/_component/VerificationModal.tsx
+++ petpal/src/app/(routes)/admin/verify/_component/VerificationModal.tsx
@@ -1,17 +1,39 @@
-import React, { useState } from "react";
+import React from "react";
+import toast from "react-hot-toast";
 
 interface Props {
   onClose: () => void;
   SVCPUsername: string | null;
+  SVCPID: string;
 }
 
-export default function VerificationModal({ onClose, SVCPUsername }: Props) {
-  const [verificationOption, setVerificationOption] = useState("");
+export default function VerificationModal({ onClose, SVCPUsername, SVCPID }: Props) {
 
-  const handleConfirm = () => {
-    // Handle confirmation action, e.g., send verification request
-    // Reset any state related to the modal
-    onClose();
+  const handleConfirm = async () => {
+    try {
+        const response = await toast.promise(
+            fetch(`http://localhost:8080/admin/serviceproviders/verify/${SVCPID}`, {
+                method: "PATCH",
+                credentials: "include",
+                body: JSON.stringify({
+                    "verify": true
+                  })
+            }),
+            {
+                loading: "Verifying service provider...",
+                success: "Service provider verified!",
+                error: "Error verifying service provider"
+            }
+        );
+        console.log(response);
+        // Close the modal after 
+        setTimeout(() => {
+            onClose();
+            window.location.reload();
+        }, 1000);
+    } catch (error) {
+        console.error("Error verifying: ", error);
+    }
   };
 
   const handleCancel = () => {
@@ -26,13 +48,13 @@ export default function VerificationModal({ onClose, SVCPUsername }: Props) {
         <span className="font-semibold text-[30px] truncate">{SVCPUsername}</span>
         <div className="flex gap-x-8">
           <button
-            className="py-2 px-5 bg-[#00FF00] text-white hover:bg-white hover:text-[#00FF00] font-semibold rounded-lg shadow-md hover:bg-orange-500"
+            className="py-2 px-5 bg-[#00FF00] text-white hover:bg-white hover:text-[#00FF00] font-semibold rounded-lg shadow-md"
             onClick={handleConfirm}
           >
             Confirm
           </button>
           <button
-            className="py-2 px-5 bg-white text-[#FF0000] hover:bg-[#FF0000]  hover:text-white font-semibold rounded-lg shadow-md hover:bg-orange-500"
+            className="py-2 px-5 bg-white text-[#FF0000] hover:bg-[#FF0000]  hover:text-white font-semibold rounded-lg shadow-md"
             onClick={handleCancel}
           >
             Cancel
--- petpal/src/app/(routes)/admin/verify/page.tsx
+++ petpal/src/app/(routes)/admin/verify/page.tsx
@@ -37,38 +37,47 @@ export default function VerifyPage() {
 
   return (
     <div className="flex flex-col items-center gap-y-5 pt-5">
-      {serviceProviders.map((serviceProvider, index) => (
-        <div
-          key={index}
-          className="flex flex-col xl:flex-row xl:justify-between xl:items-center bg-[#E8E8E8] w-[327px] xl:w-[82%] mb-4 p-[18px] rounded-xl drop-shadow-lg"
-        >
-          <div className="flex flex-col xl:flex-row xl:justify-between">
-            {/* Mobile: Service Name and Status on one row, centered. Desktop: Status moves to the left. */}
-            <div className="flex justify-between xl:justify-start items-center w-full xl:w-auto space-x-2 xl:mr-[90px]">
-              <div className="font-semibold text-[20px] xl:hidden cursor-pointer">
-                {serviceProvider.SVCPUsername}{" "}
-              </div>
-            </div>
-            {/* Provider Name: Below on mobile, in a box on the right on desktop */}
-            <div className="mt-2 xl:mt-0 xl:flex xl:items-center xl:justify-start xl:w-auto">
-              <div className="text-left">
-                <div className="font-semibold text-[20px] hidden xl:block cursor-pointer">
+      {serviceProviders === null ? (
+        <p>No unverified service provider</p>
+      ) : (
+        serviceProviders.map((serviceProvider) => (
+          <div
+            key={serviceProvider.SVCPID}
+            className="flex flex-col xl:flex-row xl:justify-between xl:items-center bg-[#E8E8E8] w-[327px] xl:w-[82%] mb-4 p-[18px] rounded-xl drop-shadow-lg"
+          >
+            <div className="flex flex-col xl:flex-row xl:justify-between">
+              {/* Mobile: Service Name and Status on one row, centered. Desktop: Status moves to the left. */}
+              <div className="flex justify-between xl:justify-start items-center w-full xl:w-auto space-x-2 xl:mr-[90px]">
+                <div className="font-semibold text-[20px] xl:hidden cursor-pointer">
                   {serviceProvider.SVCPUsername}{" "}
-                  {/* Hidden on Mobile, visible on Desktop */}
                 </div>
               </div>
-              <button
-                className="absolute right-24 bg-[#FFFFFF]"
-                onClick={() => handleVerifyClick(serviceProvider)}
-              >
-                verify svcp
-              </button>
+              {/* Provider Name: Below on mobile, in a box on the right on desktop */}
+              <div className="mt-2 xl:mt-0 xl:flex xl:items-center xl:justify-start xl:w-auto">
+                <div className="text-left">
+                  <div className="font-semibold text-[20px] hidden xl:block cursor-pointer">
+                    {serviceProvider.SVCPUsername}
+                    {/* Hidden on Mobile, visible on Desktop */}
+                  </div>
+                  <span className="font-xs">{serviceProvider.SVCPID}</span>
+                </div>
+                <button
+                  className="absolute right-10 py-2 px-5 bg-[#00FF00] text-white hover:bg-white hover:text-[#00FF00] font-semibold rounded-lg shadow-md"
+                  onClick={() => handleVerifyClick(serviceProvider)}
+                >
+                  verify svcp
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))
+      )}
       {isModalOpen && selectedServiceProvider && (
-        <VerificationModal onClose={closeModal} SVCPUsername={selectedServiceProvider.SVCPUsername} />
+        <VerificationModal
+          onClose={closeModal}
+          SVCPUsername={selectedServiceProvider.SVCPUsername}
+          SVCPID={selectedServiceProvider.SVCPID}
+        />
       )}
     </div>
   );
