ce325b05c19e2f3246d2d5b13b89939ba6589f3d
pream <preamza02@gmail.com>
2024-04-20 16:53:54 +0700

    feat: finish issue tag in chat

--- petpal/src/app/(routes)/chat/_components/IssueTag.tsx
+++ petpal/src/app/(routes)/chat/_components/IssueTag.tsx
@@ -3,21 +3,19 @@ import issueTagInterface from "../_interface/issueTagInterface"
 export default function IssueTag({ issueProps }: { issueProps: issueTagInterface }) {
     const issueId: string = issueProps.id
     const issueType: string = issueProps.type
-    const servicePrice: number = issueProps.price
     const issueDetail: string = issueProps.Detail
     const issueStatus: string = issueProps.status
     return (
         <div className={`flex flex-grow py-[10px] px-[70px]`}>
-            <div className={`flex flex-col flex-grow bg-[#FFFF00] rounded-[10px] px-[30px] py-[20px]`}>
+            <div className={`m-auto max-w-[800px] flex flex-col flex-grow bg-[#FFFF00] rounded-[10px] px-[30px] py-[20px]`}>
                 <h1 className="text-center pb-[20px] text-[24px]">{issueStatus}</h1>
                 <div className="flex flex-row">
-                    <div className="m-auto text-left w-1/2 align-top pl-[100px] outline-8 border-solid border-r-2">
+                    <div className="m-auto text-left w-1/2 align-top p-auto outline-8 border-solid border-r-2">
                         <p>Issue Id : {issueId}</p>
                         <p>type : {issueType}</p>
                         <p>Issue Date : {`${issueProps.issueDate.getDate()}/${issueProps.issueDate.getMonth()}/${issueProps.issueDate.getFullYear()} ${issueProps.issueDate.getHours()}:${issueProps.issueDate.getMinutes()}`}</p>
-                        <p>price : {servicePrice}</p>
                     </div>
-                    <div className="m-auto w-1/2 align-top px-[100px]">
+                    <div className="m-auto align-top w-1/2 pl-[50px] pr-[20px]">
                         <p>Detail :</p>
                         <p>{issueDetail}</p>
                     </div>
