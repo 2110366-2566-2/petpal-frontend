3c8f5eff14e9e79e5bd59b66eedf2a3796a2ac17
Kriszaaa <tatakrissanapat@gmail.com>
2024-04-21 15:20:21 +0700

    fix: :bug: refund api

--- petpal/src/app/libs/admin/adminApi.tsx
+++ petpal/src/app/libs/admin/adminApi.tsx
@@ -27,3 +27,19 @@ export async function getBookingById(bookingId:string){
         console.log(err)
     }
 }
+export async function refundBooking(bookingId : string){
+    try{
+        const response = await fetch(`http://localhost:8080/service/booking/payment/refund`,{
+            method:'POST',
+            credentials:'include',
+            body:JSON.stringify({
+                bookingID : bookingId
+            })
+        })
+        if(response.ok){
+            return response.json()
+        }
+    }catch(err){
+        console.log(err)
+    }
+}
